4363abc6981f6cf9c52d5187a5d65457f43765da
Gülşah Tan <fgulsahtan@gmail.com>
2026-04-19 11:52:21 +0300

    Add project files.

---
+++ week_7/Form1.Designer.cs
@@ -0,0 +1,191 @@
+﻿namespace week_7
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        private void InitializeComponent()
+        {
+            this.lblNumara = new System.Windows.Forms.Label();
+            this.lblAdSoyad = new System.Windows.Forms.Label();
+            this.lblBolum = new System.Windows.Forms.Label();
+            this.lblOgrenciListesi = new System.Windows.Forms.Label();
+            this.txtNumara = new System.Windows.Forms.TextBox();
+            this.txtAdSoyad = new System.Windows.Forms.TextBox();
+            this.comboBolum = new System.Windows.Forms.ComboBox();
+            this.listBoxOgrenciler = new System.Windows.Forms.ListBox();
+            this.btnEkle = new System.Windows.Forms.Button();
+            this.btnGuncelle = new System.Windows.Forms.Button();
+            this.btnSil = new System.Windows.Forms.Button();
+            this.btnTemizle = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumara
+            // 
+            this.lblNumara.AutoSize = true;
+            this.lblNumara.Location = new System.Drawing.Point(32, 34);
+            this.lblNumara.Name = "lblNumara";
+            this.lblNumara.Size = new System.Drawing.Size(58, 16);
+            this.lblNumara.TabIndex = 0;
+            this.lblNumara.Text = "Numara:";
+            // 
+            // lblAdSoyad
+            // 
+            this.lblAdSoyad.AutoSize = true;
+            this.lblAdSoyad.Location = new System.Drawing.Point(32, 90);
+            this.lblAdSoyad.Name = "lblAdSoyad";
+            this.lblAdSoyad.Size = new System.Drawing.Size(70, 16);
+            this.lblAdSoyad.TabIndex = 1;
+            this.lblAdSoyad.Text = "Ad Soyad:";
+            // 
+            // lblBolum
+            // 
+            this.lblBolum.AutoSize = true;
+            this.lblBolum.Location = new System.Drawing.Point(32, 147);
+            this.lblBolum.Name = "lblBolum";
+            this.lblBolum.Size = new System.Drawing.Size(48, 16);
+            this.lblBolum.TabIndex = 2;
+            this.lblBolum.Text = "Bölüm:";
+            // 
+            // lblOgrenciListesi
+            // 
+            this.lblOgrenciListesi.AutoSize = true;
+            this.lblOgrenciListesi.Location = new System.Drawing.Point(343, 34);
+            this.lblOgrenciListesi.Name = "lblOgrenciListesi";
+            this.lblOgrenciListesi.Size = new System.Drawing.Size(98, 16);
+            this.lblOgrenciListesi.TabIndex = 3;
+            this.lblOgrenciListesi.Text = "Öğrenci Listesi:";
+            // 
+            // txtNumara
+            // 
+            this.txtNumara.Location = new System.Drawing.Point(35, 53);
+            this.txtNumara.Name = "txtNumara";
+            this.txtNumara.Size = new System.Drawing.Size(235, 22);
+            this.txtNumara.TabIndex = 4;
+            this.txtNumara.TextChanged += new System.EventHandler(this.txtNumara_TextChanged);
+            // 
+            // txtAdSoyad
+            // 
+            this.txtAdSoyad.Location = new System.Drawing.Point(35, 109);
+            this.txtAdSoyad.Name = "txtAdSoyad";
+            this.txtAdSoyad.Size = new System.Drawing.Size(235, 22);
+            this.txtAdSoyad.TabIndex = 5;
+            // 
+            // comboBolum
+            // 
+            this.comboBolum.FormattingEnabled = true;
+            this.comboBolum.Location = new System.Drawing.Point(35, 166);
+            this.comboBolum.Name = "comboBolum";
+            this.comboBolum.Size = new System.Drawing.Size(235, 24);
+            this.comboBolum.TabIndex = 6;
+            // 
+            // listBoxOgrenciler
+            // 
+            this.listBoxOgrenciler.FormattingEnabled = true;
+            this.listBoxOgrenciler.ItemHeight = 16;
+            this.listBoxOgrenciler.Location = new System.Drawing.Point(346, 53);
+            this.listBoxOgrenciler.Name = "listBoxOgrenciler";
+            this.listBoxOgrenciler.Size = new System.Drawing.Size(273, 340);
+            this.listBoxOgrenciler.TabIndex = 7;
+            this.listBoxOgrenciler.SelectedIndexChanged += new System.EventHandler(this.listBoxOgrenciler_SelectedIndexChanged_1);
+            // 
+            // btnEkle
+            // 
+            this.btnEkle.Location = new System.Drawing.Point(35, 226);
+            this.btnEkle.Name = "btnEkle";
+            this.btnEkle.Size = new System.Drawing.Size(111, 35);
+            this.btnEkle.TabIndex = 8;
+            this.btnEkle.Text = "Ekle";
+            this.btnEkle.UseVisualStyleBackColor = true;
+            this.btnEkle.Click += new System.EventHandler(this.btnEkle_Click);
+            // 
+            // btnGuncelle
+            // 
+            this.btnGuncelle.Location = new System.Drawing.Point(159, 226);
+            this.btnGuncelle.Name = "btnGuncelle";
+            this.btnGuncelle.Size = new System.Drawing.Size(111, 35);
+            this.btnGuncelle.TabIndex = 9;
+            this.btnGuncelle.Text = "Güncelle";
+            this.btnGuncelle.UseVisualStyleBackColor = true;
+            this.btnGuncelle.Click += new System.EventHandler(this.btnGuncelle_Click);
+            // 
+            // btnSil
+            // 
+            this.btnSil.Location = new System.Drawing.Point(35, 274);
+            this.btnSil.Name = "btnSil";
+            this.btnSil.Size = new System.Drawing.Size(111, 35);
+            this.btnSil.TabIndex = 10;
+            this.btnSil.Text = "Sil";
+            this.btnSil.UseVisualStyleBackColor = true;
+            this.btnSil.Click += new System.EventHandler(this.btnSil_Click);
+            // 
+            // btnTemizle
+            // 
+            this.btnTemizle.Location = new System.Drawing.Point(159, 274);
+            this.btnTemizle.Name = "btnTemizle";
+            this.btnTemizle.Size = new System.Drawing.Size(111, 35);
+            this.btnTemizle.TabIndex = 11;
+            this.btnTemizle.Text = "Temizle";
+            this.btnTemizle.UseVisualStyleBackColor = true;
+            this.btnTemizle.Click += new System.EventHandler(this.btnTemizle_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(661, 430);
+            this.Controls.Add(this.btnTemizle);
+            this.Controls.Add(this.btnSil);
+            this.Controls.Add(this.btnGuncelle);
+            this.Controls.Add(this.btnEkle);
+            this.Controls.Add(this.listBoxOgrenciler);
+            this.Controls.Add(this.comboBolum);
+            this.Controls.Add(this.txtAdSoyad);
+            this.Controls.Add(this.txtNumara);
+            this.Controls.Add(this.lblOgrenciListesi);
+            this.Controls.Add(this.lblBolum);
+            this.Controls.Add(this.lblAdSoyad);
+            this.Controls.Add(this.lblNumara);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Öğrenci Yönetim Sistemi";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumara;
+        private System.Windows.Forms.Label lblAdSoyad;
+        private System.Windows.Forms.Label lblBolum;
+        private System.Windows.Forms.Label lblOgrenciListesi;
+        private System.Windows.Forms.TextBox txtNumara;
+        private System.Windows.Forms.TextBox txtAdSoyad;
+        private System.Windows.Forms.ComboBox comboBolum;
+        private System.Windows.Forms.ListBox listBoxOgrenciler;
+        private System.Windows.Forms.Button btnEkle;
+        private System.Windows.Forms.Button btnGuncelle;
+        private System.Windows.Forms.Button btnSil;
+        private System.Windows.Forms.Button btnTemizle;
+    }
+}
---
+++ week_7/Form1.cs
@@ -0,0 +1,151 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+namespace week_7
+{
+    public partial class Form1 : Form
+    {
+        public class Student
+        {
+            public int Id { get; set; }
+            public int Numara { get; set; }
+            public string AdSoyad { get; set; }
+            public string Bolum { get; set; }
+            
+        }
+
+        private List<Student> ogrenciler = new List<Student>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBolum.Items.Add("YBS");
+            comboBolum.Items.Add("Bilg.");
+            comboBolum.Items.Add("End.");
+
+            ogrenciler.Add(new Student { Id = 1, Numara = 101, AdSoyad = "Ali Yılmaz", Bolum = "BST" });
+            ogrenciler.Add(new Student { Id = 2, Numara = 102, AdSoyad = "Ayşe Demir", Bolum = "Bilg." });
+            ogrenciler.Add(new Student { Id = 3, Numara = 103, AdSoyad = "Mehmet Kaya", Bolum = "End." });
+
+            ListeyiYenile();
+
+            listBoxOgrenciler.SelectedIndexChanged += listBoxOgrenciler_SelectedIndexChanged;
+        }
+        private Student SeciliOgrenciyiGetir()
+        {
+            int index = listBoxOgrenciler.SelectedIndex;
+
+            if (index == -1)
+                return null;
+
+            return ogrenciler[index];
+        }
+        private void ListeyiYenile()
+        {
+            listBoxOgrenciler.Items.Clear();
+
+            foreach (var ogr in ogrenciler)
+            {
+                listBoxOgrenciler.Items.Add(
+                    ogr.Numara + " - " + ogr.AdSoyad + " (" + ogr.Bolum + ")"
+                );
+            }
+        }
+
+        private void btnEkle_Click(object sender, EventArgs e)
+        {
+            if (!int.TryParse(txtNumara.Text, out int numara))
+            {
+                MessageBox.Show("Numara sayısal olmalı");
+                return;
+            }
+
+            Student yeni = new Student
+            {
+                Id = ogrenciler.Count + 1,
+                Numara = numara,
+                AdSoyad = txtAdSoyad.Text,
+                Bolum = comboBolum.Text
+            };
+
+            ogrenciler.Add(yeni);
+            ListeyiYenile();
+        }
+        private void btnGuncelle_Click(object sender, EventArgs e)
+        {
+            Student secili = SeciliOgrenciyiGetir();
+
+            if (secili == null)
+            {
+                MessageBox.Show("Öğrenci seçiniz");
+                return;
+            }
+
+            if (!int.TryParse(txtNumara.Text, out int numara))
+            {
+                MessageBox.Show("Numara sayısal olmalı");
+                return;
+            }
+
+            secili.Numara = numara;
+            secili.AdSoyad = txtAdSoyad.Text;
+            secili.Bolum = comboBolum.Text;
+
+            ListeyiYenile();
+        }
+        private void btnSil_Click(object sender, EventArgs e)
+        {
+            int index = listBoxOgrenciler.SelectedIndex;
+
+            if (index == -1)
+            {
+                MessageBox.Show("Öğrenci seçiniz");
+                return;
+            }
+
+            ogrenciler.RemoveAt(index);
+            ListeyiYenile();
+        }
+
+        private void btnTemizle_Click(object sender, EventArgs e)
+        {
+            TemizleGiris();
+        }
+
+        private void listBoxOgrenciler_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            Student secili = SeciliOgrenciyiGetir();
+
+            if (secili == null)
+                return;
+
+            txtNumara.Text = secili.Numara.ToString();
+            txtAdSoyad.Text = secili.AdSoyad;
+            comboBolum.Text = secili.Bolum;
+        }
+
+        private void TemizleGiris()
+        {
+            txtNumara.Clear();
+            txtAdSoyad.Clear();
+            comboBolum.SelectedIndex = -1;
+            listBoxOgrenciler.ClearSelected();
+            txtNumara.Focus();
+        }
+
+        private void txtNumara_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void listBoxOgrenciler_SelectedIndexChanged_1(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
